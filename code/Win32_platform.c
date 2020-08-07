d6478c28e539bfb76c8c2472e28ed3ca83c2ed94
GeraldRyan <gerald.ryan40@yahoo.com>
2020-08-06 22:57:06 -0500

    making progress learning C and the coding process. So interesting. I am tired of high level languages. I want to get down to the bedrock, as well as learn libraries

--- code/Win32_platform.c
+++ code/Win32_platform.c
@@ -1,35 +1,56 @@
 #include <windows.h>
+#include <stdint.h>
+#include "utils.c"
 
-typedef int b32;
+struct
+{
+  int width, height;
+  u32 *pixels;
+  BITMAPINFO bitmap_info;
+} typedef Render_Buffer;
 
-#define true 1
-#define false 0
-
-#define global_variable static
-#define internal static
-
-global_variable b32 running = true;
-
+Render_Buffer render_buffer;
 // messages come here
 internal LRESULT CALLBACK window_callback(HWND window, UINT message, WPARAM w_param, LPARAM l_param)
 {
 
-
   LRESULT result = 0;
 
-  switch(message){
-    case WM_CLOSE:
-    case WM_DESTROY: {
-      running = false;
-    } break;
+  switch (message)
+  {
+  case WM_CLOSE:
+  case WM_DESTROY:
+  {
+    running = false;
+  }
+  break;
+  case WM_SIZE:{
+    // Get width and height
+    RECT rect;
+    GetWindowRect(window, &rect);
+    render_buffer.width = rect.right - rect.left;
+    render_buffer.height = rect.top - rect.bottom; 
 
-    default: {
-       result = DefWindowProcA(window, message, w_param, l_param);
+    if (render_buffer.pixels){
+      // free 
     }
 
+    render_buffer.pixels = VirtualAlloc(0, sizeof(u32)*render_buffer.width * render_buffer.height,MEM_COMMIT|MEM_RESERVE, PAGE_READWRITE
+);
+
+
+    // allocate buffer
+
+    // fill the bitmapinfo
+  } break;
+
+  default:
+  {
+    result = DefWindowProcA(window, message, w_param, l_param);
+  }
   }
 
-  return result; 
+  return result;
 };
 
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
@@ -46,17 +67,23 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 
   HWND window = CreateWindowExA(0, window_class.lpszClassName, "Breakout", WS_VISIBLE | WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, 0, 0);
 
-  while (running) {
+  HDC hdc = GetDC(window);
+
+  while (running)
+  {
     // input
     MSG message;
-    while (PeekMessageA(&message, window, 0,0, PM_REMOVE)){
+    while (PeekMessageA(&message, window, 0, 0, PM_REMOVE))
+    {
       TranslateMessage(&message);
       DispatchMessage(&message);
     }
 
     // simulation
-
+    int width, height;
+    void *memory;
+    BITMAPINFO bitmap_info;
     // render
+    StretchDIBits(hdc, 0, 0, render_buffer.width, render_buffer.height, 0, 0, render_buffer.width, render_buffer.height, render_buffer.pixels, &render_buffer.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
   }
-
 }
--- code/build.bat
+++ code/build.bat
@@ -1,5 +1,9 @@
 echo off
 pushd ..\build
-cl -nologo -Zi -FC ..\code\win32_platform.c user32.lib
+del *.pdb > NUL 2> NUL
+echo WAITING FOR PDB > lock.tmp
+cl -nologo -Zi -FC ..\code\win32_platform.c /link user32.lib gdi32.lib -incremental:no -opt:ref
 
+del lock.tmp
+del *.obj
 popd
